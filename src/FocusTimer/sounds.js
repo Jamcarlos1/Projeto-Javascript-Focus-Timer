57f7f57014c7fd7a5c7e3db3af84b40cb807c0b7
Jamcarlos1 <165224850+Jamcarlos1@users.noreply.github.com>
2024-07-02 21:58:21 -0300

    Add files via upload

---
+++ src/FocusTimer/sounds.js
@@ -0,0 +1,4 @@
+export const buttonPressAudio = new Audio('./Assets/button-press.wav')
+export const kitchenTimer = new Audio('./Assets/kichen-timer.mp3')
+export const bgAudio = new Audio('./Assets/bg-audio.mp3')
+
